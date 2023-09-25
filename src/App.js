0cce8354449c1428b74997fe1f6839482555283d
rin <sayuri.shindo@gmail.com>
2023-09-25 17:42:27 +0900

    マスの値をpropsで渡す

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // 各マスの正方形を生成するコンポーネントを定義
-function Square() {
-  return <button className="square">1</button>;
+// props:value
+function Square( {value} ) {
+  return <button className="square">{ value }</button>;
 }
 
 // 外部から参照できるメイン関数"Square"を定義
@@ -12,21 +13,22 @@ export default function Board() {
       {/* 3行描写 */}
       <div className="board-row">
         {/* Squareコンポーネントを3マス分呼び出す */}
-        <Square />
-        <Square />
-        <Square />
+        {/* 表示する値はvalueで指定 */}
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
         {/* Squareコンポーネントを3マス分呼び出す */}
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
         {/* Squareコンポーネントを3マス分呼び出す */}
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
