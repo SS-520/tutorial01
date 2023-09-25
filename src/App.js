0fbefa8b0da156ac058066b32e744e0c4656e753
rin <sayuri.shindo@gmail.com>
2023-09-25 15:53:39 +0900

    マスを追加 1マスから3×3の9マスへ変更

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 // 外部から参照できるメイン関数"Square"を定義
-export default function Square() {
-  // クラス名"square"のボタンタグを返す（JSX）
-  return <button className="square">X</button>;
+// "Square"ではなくなったので"Board"に変更
+export default function Board() {
+  return (
+    // JSXを返すので空の親要素<></>を設定
+    <>
+      {/* 3×3の格子とナンバリング */}
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  );
 }
