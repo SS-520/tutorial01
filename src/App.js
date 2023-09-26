896f8fb1b6ace6134084750618a7a238500b5332
rin <sayuri.shindo@gmail.com>
2023-09-26 13:09:55 +0900

    マスの中身をuseStateで定義

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+// useState関数をimport
+import { useState } from "react";
+
 // 各マスの正方形を生成するコンポーネントを定義
 // props:value
-function Square({ value }) {
-  
+// マスの中身を外部から受け取らず内部のsetValueで規定に変更
+function Square() {
+
+  // 値valuseを永続定義
+  // valueの中身はuseStateを設定することでsetValueの中身で変更
+  const [value, setValue] = useState(null);
+
   // consoleに"clicked!"が表示されるようにする
   function handleClick() {
-    console.log("clicked!");
+    // console.log("clicked!");
+    // setValueに値"X"を設定
+    // この関数が呼び出されるたびにvalueの中身が変更されて再レンダリングが走る
+    setValue('X');
   }
   
   // マスをクリックしたらhandleClick()を呼び出す
@@ -16,26 +27,27 @@ function Square({ value }) {
 export default function Board() {
   return (
     // JSXを返すので空の親要素<></>を設定
+    // Square呼び出し時のvalueを削除
     <>
       {/* 3行描写 */}
       <div className="board-row">
         {/* Squareコンポーネントを3マス分呼び出す */}
         {/* 表示する値はvalueで指定 */}
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
         {/* Squareコンポーネントを3マス分呼び出す */}
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
         {/* Squareコンポーネントを3マス分呼び出す */}
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
